36d718fdb5e6782d7f258ca91ca26df634db2dae
u7chan <34462401+u7chan@users.noreply.github.com>
2024-05-19 18:32:42 +0900

    fix: remove react-audio-stream

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { useAudioStream } from "react-audio-stream";
+import { useAudioStream } from "./useAudioStream";
 
 export default function Page() {
   const [messages, setMessages] = useState<string[]>([]);
